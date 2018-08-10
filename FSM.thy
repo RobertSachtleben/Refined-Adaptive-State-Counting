c765f8d442616e7f2b830729a8263e16ba2dd623
RSA <rsachtl@tzi.de>
2018-08-10 18:50:58 +0200

    initial FSM definitions

---
+++ FSM.thy
@@ -0,0 +1,46 @@
+theory FSM
+  imports Main
+begin
+
+record ('in, 'out, 'state) FSM =
+  states  :: "'state set"
+  initial :: "'state"
+  inputs  :: "'in set"
+  outputs :: "'out set"
+  transitions :: "('state * 'in * 'out * 'state) set"
+
+definition well_formed :: "('in, 'out, 'state) FSM \<Rightarrow> bool" where
+"well_formed M \<equiv> 
+    finite (states M)
+  \<and> initial M \<in> states M
+  \<and> finite (inputs M)
+  \<and> finite (outputs M)
+  \<and> (\<forall> (s1,x,y,s2) \<in> transitions M . 
+      s1 \<in> states M \<and> s2 \<in> states M \<and> x \<in> inputs M \<and> y \<in> outputs M)"
+
+fun h :: "('in, 'out, 'state) FSM \<Rightarrow> 'state \<Rightarrow> 'in \<Rightarrow> ('out * 'state) set" where
+"h M s1 x = { (y,s2) | y s2 . (s1,x,y,s2) \<in> transitions M }"
+
+fun h_out :: "('in, 'out, 'state) FSM \<Rightarrow> 'state \<Rightarrow> 'in \<Rightarrow> 'out set" where
+"h_out M s1 x = { fst r | r . r \<in> h M s1 x }"
+
+fun h_reach :: "('in, 'out, 'state) FSM \<Rightarrow> 'state \<Rightarrow> 'in \<Rightarrow> 'state set" where
+"h_reach M s1 x = { snd r | r . r \<in> h M s1 x }"
+
+fun h_y :: "('in, 'out, 'state) FSM \<Rightarrow> 'state \<Rightarrow> 'in \<Rightarrow> 'out \<Rightarrow> 'state set" where
+"h_y M s1 x y = { s2 . (y,s2) \<in> h M s1 x }"
+
+
+
+definition observable :: "('in, 'out, 'state) FSM \<Rightarrow> bool" where
+"observable M \<equiv> \<forall> s1 \<in> states M . \<forall> x \<in> inputs M . \<forall> y \<in> outputs M . card (h_y M s1 x y) \<le> 1"
+
+definition completely_specified :: "('in, 'out, 'state) FSM \<Rightarrow> bool" where
+"completely_specified M \<equiv> \<forall> s1 \<in> states M . \<forall> x \<in> inputs M . \<exists> y \<in> outputs M . \<exists> s2 \<in> states M . (s1,x,y,s2) \<in> transitions M"
+
+definition deterministic :: "('in, 'out, 'state) FSM \<Rightarrow> bool" where
+"deterministic M \<equiv> \<forall> s1 \<in> states M . \<forall> x \<in> inputs M . \<forall> (ya,s2a) \<in> h M s1 x . \<forall> (yb,s2b) \<in> h M s1 x . (ya,s2a) = (yb,s2b)"
+
+
+
+end
