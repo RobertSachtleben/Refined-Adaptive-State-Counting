efd0613fc65c7eb29457fda03e0fcf618e33cdbb
RSA <rsachtl@tzi.de>
2018-08-12 15:00:48 +0200

    proof of uniqueness of io sequences under observability

--- FSM.thy
+++ FSM.thy
@@ -69,6 +69,19 @@ next
   qed
 qed
 
+lemma sequence_transition :
+  assumes is_seq : "is_sequence M seq"
+  assumes is_idx : "n < length seq"
+  assumes at_idx : "a = seq ! n"
+  shows "a \<in> transitions M"
+proof -
+  let ?fromIdx = "drop n seq"
+  have "is_sequence M ?fromIdx" using assms by (simp add: sequence_drop)
+  moreover have "a = hd ?fromIdx" using assms by (simp add: hd_drop_conv_nth)
+  ultimately show "a \<in> transitions M" using assms by (metis Cons_nth_drop_Suc is_sequence.simps(2) is_sequence.simps(3) length_drop length_greater_0_conv zero_less_diff)
+qed
+  
+
 
 fun is_enabled_sequence :: "('in, 'out, 'state) FSM \<Rightarrow> 'state \<Rightarrow> ('state * 'in * 'out * 'state) list => bool" where
 "is_enabled_sequence M s Nil = True" |
@@ -81,7 +94,9 @@ fun enabled_sequences :: "('in, 'out, 'state) FSM \<Rightarrow> 'state \<Rightar
 "enabled_sequences M s = {seq . is_enabled_sequence M s seq}"
 
 fun get_io :: "('state * 'in * 'out * 'state) list \<Rightarrow> ('in * 'out) list" where
-"get_io seq = zip (map t_input seq) (map t_output seq)"
+"get_io Nil = Nil" |
+"get_io ((s1,x,y,s2)#seq) = (x,y) # get_io seq"
+(*"get_io seq = zip (map t_input seq) (map t_output seq)"*)
 (*"get_io seq = map (\<lambda> (s1,x,y,s2) . (x,y)) seq"*)
 
 definition is_enabled_io_sequence :: "('in, 'out, 'state) FSM \<Rightarrow> 'state \<Rightarrow> ('in * 'out) list => bool" where
@@ -162,71 +177,7 @@ lemma get_io_length_eq :
   shows "length seq1 = length seq2"
   by (metis assms get_io_length)
   
-(*
-lemma get_io_length : "
-  well_formed M 
-  \<Longrightarrow> is_enabled_sequence M s seq 
-  \<Longrightarrow> io = get_io seq 
-  \<Longrightarrow> is_enabled_sequence M s seq2  
-  \<Longrightarrow> io = get_io seq2 
-  \<Longrightarrow> length seq = length seq2"
-  apply (simp)
-  by (metis length_map)
 
-lemma observable_sequence1 : "
-  well_formed M 
-  \<Longrightarrow> observable M 
-  \<Longrightarrow> is_enabled_sequence M s ((s1,x11,y11,s2)#S1)
-  \<Longrightarrow> is_enabled_sequence M s ((t1,x12,y12,t2)#S2)  
-  \<Longrightarrow> get_io ((s1,x11,y11,s2)#S1) = get_io ((t1,x12,y12,t2)#S2)  
-  \<Longrightarrow> (s1,x11,y11,s2) = (t1,x12,y12,t2)"
-  by (auto simp: well_formed_def observable_def)
-
-lemma observable_sequence1c : "
-  well_formed M 
-  \<Longrightarrow> observable M 
-  \<Longrightarrow> is_enabled_sequence M s ((s1,x11,y11,s2)#S1)
-  \<Longrightarrow> is_enabled_sequence M s ((t1,x12,y12,t2)#S2)  
-  \<Longrightarrow> get_io ((s1,x11,y11,s2)#S1) = get_io ((t1,x12,y12,t2)#S2)  
-  \<Longrightarrow> S1 = S2
-  \<Longrightarrow> ((s1,x11,y11,s2)#S1) = ((t1,x12,y12,t2)#S2)"
-  by (auto simp: well_formed_def observable_def)
-
-lemma observable_sequence1d : "
-  well_formed M 
-  \<Longrightarrow> observable M 
-  \<Longrightarrow> is_enabled_sequence M s (a1#S1)
-  \<Longrightarrow> is_enabled_sequence M s (a2#S2)  
-  \<Longrightarrow> get_io (a1#S1) = get_io (a2#S2)  
-  \<Longrightarrow> S1 = S2
-  \<Longrightarrow> (a1#S1) = (a2#S2)"
-  apply (cases a1)
-  apply (cases a2)
-  by (auto simp: well_formed_def observable_def)
-
-lemma observable_sequence1d2 : "
-  well_formed M 
-  \<Longrightarrow> observable M 
-  \<Longrightarrow> is_enabled_sequence M s (a1#S1)
-  \<Longrightarrow> is_enabled_sequence M s (a2#S2)  
-  \<Longrightarrow> get_io (a1#S1) = get_io (a2#S2)  
-  \<Longrightarrow> a1 = a2"
-  apply (cases a1)
-  apply (cases a2)
-  by (auto simp: well_formed_def observable_def)
-  
-
-lemma observable_sequence1e : "
-  well_formed M 
-  \<Longrightarrow> observable M 
-  \<Longrightarrow> is_enabled_sequence M s (a1#S1)
-  \<Longrightarrow> is_enabled_sequence M s S2  
-  \<Longrightarrow> get_io (a1#S1) = get_io S2  
-  \<Longrightarrow> S1 = tl S2
-  \<Longrightarrow> (a1#S1) = S2"
-  apply (cases a1)
-  by (auto simp: well_formed_def observable_def)
-*)
 
 lemma first_list_diff_zip :
   fixes zipSeq :: "('a * 'a) list"
@@ -280,18 +231,33 @@ proof -
   ultimately show ?thesis using assms by (metis Cons_nth_drop_Suc Suc_lessD is_sequence.simps(3))
 qed 
 
-lemma get_io_index :
+lemma get_io_input_index : "\<forall> j . ((j < length seq) \<and> (length seq > 0)) \<longrightarrow> t_input (seq ! j) = fst ((get_io seq) ! j)"
+  apply (induction seq)
+  apply auto
+  using less_Suc_eq_0_disj by auto
+
+lemma get_io_output_index : "\<forall> j . ((j < length seq) \<and> (length seq > 0)) \<longrightarrow> t_output (seq ! j) = snd ((get_io seq) ! j)"
+  apply (induction seq)
+  apply auto
+  using less_Suc_eq_0_disj by auto
+
+lemma get_io_index_eq :
   assumes en : "get_io seq1 = get_io seq2"
   assumes idx: "j < length seq1"
+  assumes nonempty: " length seq1 > 0"
   shows "t_input (seq1 ! j) = t_input (seq2 ! j) \<and> t_output (seq1 ! j) = t_output (seq2 ! j)"
-proof 
-  have "map t_input seq1 = map t_input seq2" using en by (metis get_io.simps length_map map_fst_zip)
-  then show "t_input (seq1 ! j) = t_input (seq2 ! j)" by (metis idx map_eq_imp_length_eq nth_map)
-  have "map t_output seq1 = map t_output seq2" using en by (metis get_io.simps length_map map_snd_zip)
-  then show "t_output (seq1 ! j) = t_output (seq2 ! j)" by (metis idx map_eq_imp_length_eq nth_map)
+proof -
+  have "length seq1 = length seq2" using assms get_io_length_eq by blast
+  moreover have "t_input (seq1 ! j) = fst ((get_io seq1) ! j)" using assms by (simp add: get_io_input_index)
+  moreover have "t_input (seq2 ! j) = fst ((get_io seq2) ! j)" using assms by (simp add: calculation(1) get_io_input_index)
+  moreover have "t_input (seq1 ! j) = t_input (seq2 ! j)" using assms by (simp add: calculation(2) calculation(3))
+  moreover have "t_output (seq1 ! j) = snd ((get_io seq1) ! j)" using assms by (simp add: get_io_output_index)
+  moreover have "t_output (seq2 ! j) = snd ((get_io seq2) ! j)" using assms by (simp add: calculation(1) get_io_output_index)
+  moreover have "t_output (seq1 ! j) = t_output (seq2 ! j)" using assms by (simp add: calculation(5) calculation(6))
+  ultimately show ?thesis by auto
 qed
 
-theorem test:
+theorem observable_unique_io:
   assumes wf:"well_formed M" 
   assumes ob:"observable M"  
   assumes e1:"is_enabled_sequence M s seq1" 
@@ -301,167 +267,48 @@ theorem test:
     "seq1 = seq2"
 proof (rule ccontr)
   assume ineq: "seq1 \<noteq> seq2"
+  have nonempty: "length seq1 > 0" using assms get_io_length_eq ineq by fastforce
   have sameLength : "length seq1 = length seq2" by (metis get_io_length io)
   then obtain mdi where mdi_def : (* minimal difference index *)
         "mdi < length seq1 
           \<and> seq1 ! mdi \<noteq> seq2 ! mdi 
           \<and> (\<forall> i . i < mdi \<longrightarrow> seq1 ! i = seq2 ! i)" using first_list_diff ineq by blast
   then have "take mdi seq1 = take mdi seq2" by (simp add: nth_take_lemma sameLength)
-  let ?equalInit = "take mdi seq1"
-  let ?diff1 = "seq1 ! mdi"
-  let ?diff2 = "seq2 ! mdi"
-  
-  have "False"
-  proof (cases mdi)
+  have seq1_trans : "seq1 ! mdi \<in> transitions M" using assms by (metis is_enabled_sequence.elims(2) length_greater_0_conv mdi_def nonempty sequence_transition)
+  have seq2_trans : "seq2 ! mdi \<in> transitions M" using assms by (metis is_enabled_sequence.elims(2) length_greater_0_conv mdi_def sameLength sequence_transition)
+  obtain s1 xa ya s2 where diff1_def : "seq1 ! mdi = (s1,xa,ya,s2)" using prod_cases4 by blast
+  obtain t1 xb yb t2 where diff2_def : "seq2 ! mdi = (t1,xb,yb,t2)" using prod_cases4 by blast
+    
+  show "False"
+  proof (cases mdi) (* rework case split, as both cases only differ meaningfully in the proof of s1=t1 *)
     case 0
-    obtain s1 xa ya s2 where diff1_def : "seq1 ! mdi = (s1,xa,ya,s2)" using prod_cases4 by blast
-    obtain t1 xb yb t2 where diff2_def : "seq2 ! mdi = (t1,xb,yb,t2)" using prod_cases4 by blast
     have "s1 = t1" using assms by (metis "0" diff1_def diff2_def fst_conv is_enabled_sequence.elims(2) list.size(3) mdi_def nth_Cons_0 sameLength)
-    moreover have "xa = xb" using diff1_def diff2_def get_io_index mdi_def by fastforce
-      (*using assms by (smt "0" Suc_length_conv gr0_conv_Suc mdi_def nth_Cons_0 observable_sequence1d2 sameLength)*)
-    moreover have "ya = yb" using assms by (smt "0" Suc_length_conv gr0_conv_Suc mdi_def nth_Cons_0 observable_sequence1d2 sameLength)
-    moreover have "s2 = t2" using assms by (smt "0" Suc_length_conv gr0_conv_Suc mdi_def nth_Cons_0 observable_sequence1d2 sameLength)
+    moreover have "xa = xb" using assms using "0" diff1_def diff2_def get_io_index_eq mdi_def by fastforce
+    moreover have "ya = yb" using assms by (metis "0" diff1_def diff2_def get_io_output_index mdi_def sameLength t_output.simps)
+    moreover have "(s1,xa,ya,s2) \<in> transitions M" using diff1_def seq1_trans by auto
+    moreover have "(s1,xa,ya,t2) \<in> transitions M" using calculation(1) calculation(2) calculation(3) diff2_def seq2_trans by auto
+    moreover have "s2 = t2" using calculation(4) calculation(5) ob observable_def by fastforce
     ultimately have "seq1 ! mdi = seq2 ! mdi" by (simp add: diff1_def diff2_def)
     moreover have "seq1 ! mdi \<noteq> seq2 ! mdi" using mdi_def by auto
     ultimately show ?thesis by auto
   next
     case (Suc lastSame)
-    
-    obtain s1 xa ya s2 where diff1_def : "seq1 ! mdi = (s1,xa,ya,s2)" using prod_cases4 by blast
-    obtain t1 xb yb t2 where diff2_def : "seq2 ! mdi = (t1,xb,yb,t2)" using prod_cases4 by blast
     obtain s0 xp yp s1p where same_def : "seq1 ! lastSame = (s0,xp,yp,s1p)" using prod_cases4 by blast
     have same_last : "seq1 ! lastSame = seq2 ! lastSame" using mdi_def by (simp add: Suc)
-    
-    have io_eq_index : "t_input (seq1 ! mdi) = t_input (seq2 ! mdi)" using assms by sledgehamme
     have "s1 = s1p" using assms sequence_source_target by (metis Suc diff1_def is_enabled_sequence.elims(2) list.size(3) mdi_def not_less0 same_def t_source.simps t_target.simps)
-    moreover have "xa = xb" using assms by sledgehamme
-    moreover have "ya = yb" using assms by (smt "0" Suc_length_conv gr0_conv_Suc mdi_def nth_Cons_0 observable_sequence1d2 sameLength)
-    then show ?thesis sorry
-  qed
-  
-  
-  ultimately obtain minDiffIndex where mdi_def :
-      "minDiffIndex \<le> length seq1 
-        \<and> seq1 ! minDiffIndex \<noteq> seq2 ! minDiffIndex
-        \<and> (\<forall> (i::nat) . (i < minDiffIndex) \<longrightarrow> (seq1 ! i = seq2 ! i))" by sledgehammer
-  
-  then have "?diffs \<noteq> {}" by auto
-  then have "\<forall> (i::nat) . (i < minDiffIndex) \<longrightarrow> take i seq1 = take i seq2" by sledgehamme
-
-lemma observable_sequence : "
-  well_formed M 
-  \<Longrightarrow> observable M 
-  \<Longrightarrow> is_enabled_sequence M s seq1 
-  \<Longrightarrow> is_enabled_sequence M s seq2  
-  \<Longrightarrow> get_io seq1 = get_io seq2 
-  \<Longrightarrow> seq1 = seq2"
-proof -
-  let ?zipList = "zip seq1 seq2"
-  have "length seq1 = length seq2" using wf by (metis get_io.simps length_map)
-  moreover have "seq1 = map fst ?zipList" by (simp add: calculation)
-  moreover have "seq2 = map snd ?zipList" by (simp add: calculation)
-  ultimately have ?thesis 
-  proof (induction ?zipList)
-    case Nil
-    then show ?case by auto
-  next
-    case (Cons  a x)
-    obtain 
-    then show ?case sorry
-  qed
-qed
-
-
-theorem test:
-  assumes wf:"well_formed M" 
-  assumes ob:"observable M"  
-  assumes e1:"is_enabled_sequence M s seq" 
-  assumes e2:"is_enabled_sequence M s seq2 "
-  assumes io:"get_io seq = get_io seq2 "
-  shows
-    "seq = seq2"
-proof -
-  have "length seq = length seq2" using wf by (metis get_io.simps length_map)
-  then obtain n where n_def : "n = length seq \<and> n = length seq2" by simp
-  
-  then show ?thesis using assms
-  proof (induction n)
-    case 0
-    then show ?case by auto
-  next
-    case (Suc n)
-    obtain a1 S1 where seq1_head : "seq = (a1#S1)" by (meson Suc.prems(1) Suc_length_conv)
-    obtain a2 S2 where seq2_head : "seq2 = (a2#S2)" by (meson Suc.prems(1) Suc_length_conv)
-    have "n = length S1" using Suc.prems(1) seq1_head by auto 
-    moreover have "n = length S2" using Suc.prems(1) seq2_head by auto
-    
-print_theorems
-
-    ultimately have "S1 = S2" using Suc.IH by sledgehammer
-    then show ?case
+    moreover have "t1 = s1p" using assms sequence_source_target by (metis Suc diff2_def is_enabled_sequence.elims(2) length_greater_0_conv mdi_def sameLength same_def same_last t_source.simps t_target.simps)
+    moreover have "s1 = t1" by (simp add: calculation(1) calculation(2))
+    moreover have "xa = xb" using assms diff1_def diff2_def get_io_index_eq mdi_def by fastforce
+    moreover have "ya = yb" using diff1_def diff2_def by (metis get_io_output_index io mdi_def nonempty sameLength t_output.simps)
+    moreover have "(s1,xa,ya,s2) \<in> transitions M" using diff1_def seq1_trans by auto
+    moreover have "(s1,xa,ya,t2) \<in> transitions M" using calculation(3) calculation(4) calculation(5) diff2_def seq2_trans by auto
+    moreover have "s2 = t2" using calculation(6) calculation(7) ob observable_def by fastforce
+    ultimately have "seq1 ! mdi = seq2 ! mdi" by (simp add: diff1_def diff2_def)
+    moreover have "seq1 ! mdi \<noteq> seq2 ! mdi" using mdi_def by auto
+    ultimately show ?thesis by auto
   qed
 qed
   
 
-
-lemma observable_sequence : "
-  well_formed M 
-  \<Longrightarrow> observable M 
-  \<Longrightarrow> is_enabled_sequence M s seq 
-  \<Longrightarrow> is_enabled_sequence M s seq2  
-  \<Longrightarrow> get_io seq = get_io seq2 
-  \<Longrightarrow> seq = seq2"
-proof (induction seq)
-  case Nil
-  then show ?case sorry
-next
-  case (Cons a seq)
-  then show ?case sorry
-qed
-  have "length seq = length seq2" by (meson get_io_length)
-  then obtain n where n_def : "n = length seq \<and> n = length seq2" by (meson get_io_length)
-qed
-
-
-
-
-
-  let ?n = "length seq"
-  then show ?thesis 
-  proof (induction ?n)
-    case 0
-    then show ?case sledgehammer
-  next
-    case (Suc x)
-    then show ?case sorry
-  qed
-  case Nil
-  then show ?case by auto
-next
-  case (Cons a seq)
-  
-  
-
-  (*then have "length (a # seq) = length seq2" by (meson get_io_length)*)
-  then show ?case sorry
-qed
- 
-
-  apply (induction seq)
-   apply (simp)
-  apply (frule get_io_length)
-  apply auto
-
-  apply (frule observable_sequence1e)
-       apply auto
-  apply sledgehammer
-
-  apply (cases seq2)
-   apply (simp add: well_formed_def observable_def)
-  apply auto
-  apply sledgehammer
-   apply (simp add: observable_def)
-  apply sledgehammer
-apply (auto simp: well_formed_def observable_def)
 
 end
