87afa20ce0973722076bcbe76d7803e6ce6c92b2
RSA <rsachtl@tzi.de>
2018-08-12 03:02:31 +0200

    rework get_io

--- FSM.thy
+++ FSM.thy
@@ -27,9 +27,53 @@ reaches_nil : "reaches M Nil (initial M)" |
 reaches_transition : "reaches M seq s1 \<Longrightarrow> (s1,x,y,s2) \<in> transitions M \<Longrightarrow> reaches M (seq@((x,y)#Nil)) s2"
 *)
 
+fun t_source :: "('state * 'in * 'out * 'state) \<Rightarrow> 'state" where
+"t_source (s1,x,y,s2) = s1"
+
+fun t_input :: "('state * 'in * 'out * 'state) \<Rightarrow> 'in" where
+"t_input (s1,x,y,s2) = x"
+
+fun t_output :: "('state * 'in * 'out * 'state) \<Rightarrow> 'out" where
+"t_output (s1,x,y,s2) = y"
+
+fun t_target :: "('state * 'in * 'out * 'state) \<Rightarrow> 'state" where
+"t_target (s1,x,y,s2) = s2"
+
+
+fun is_sequence :: "('in, 'out, 'state) FSM \<Rightarrow> ('state * 'in * 'out * 'state) list \<Rightarrow> bool" where
+"is_sequence M Nil = True" |
+"is_sequence M (a#Nil) = (a \<in> transitions M)" |
+"is_sequence M (a#b#seq) = (a \<in> transitions M \<and> t_target a = t_source b \<and> is_sequence M (b#seq))" 
+
+
+
+lemma sequence_drop :
+  assumes "is_sequence M seq"
+  shows "is_sequence M (drop n seq)"
+proof (induction n)
+  case 0
+  then show ?case using assms by auto
+next
+  case (Suc n)
+  then show ?case 
+  proof (cases "drop (Suc n) seq")
+    case Nil
+    then show ?thesis by simp
+  next
+    case (Cons a list)
+    let ?d1 = "drop n seq"
+    let ?dh = "hd ?d1"
+    let ?d2 = "drop (Suc n) seq"
+    have "?d1 = ?dh # ?d2" by (metis drop_Suc drop_eq_Nil le_SucI list.exhaust_sel list.simps(3) local.Cons tl_drop)
+    then show ?thesis by (metis Suc.IH is_sequence.simps(3) local.Cons)
+  qed
+qed
+
+
 fun is_enabled_sequence :: "('in, 'out, 'state) FSM \<Rightarrow> 'state \<Rightarrow> ('state * 'in * 'out * 'state) list => bool" where
 "is_enabled_sequence M s Nil = True" |
-"is_enabled_sequence M s ((s1,x,y,s2)#seq) = (s = s1 \<and> (s1,x,y,s2) \<in> transitions M \<and> is_enabled_sequence M s2 seq)"
+(*"is_enabled_sequence M s ((s1,x,y,s2)#seq) = (s = s1 \<and> (s1,x,y,s2) \<in> transitions M \<and> is_enabled_sequence M s2 seq)"*)
+"is_enabled_sequence M s (a#seq) = ((fst a = s) \<and> is_sequence M (a#seq))"
 
 print_theorems
 
@@ -37,7 +81,8 @@ fun enabled_sequences :: "('in, 'out, 'state) FSM \<Rightarrow> 'state \<Rightar
 "enabled_sequences M s = {seq . is_enabled_sequence M s seq}"
 
 fun get_io :: "('state * 'in * 'out * 'state) list \<Rightarrow> ('in * 'out) list" where
-"get_io seq = map (\<lambda> (s1,x,y,s2) . (x,y)) seq"
+"get_io seq = zip (map t_input seq) (map t_output seq)"
+(*"get_io seq = map (\<lambda> (s1,x,y,s2) . (x,y)) seq"*)
 
 definition is_enabled_io_sequence :: "('in, 'out, 'state) FSM \<Rightarrow> 'state \<Rightarrow> ('in * 'out) list => bool" where
 "is_enabled_io_sequence M s io \<equiv> \<exists> seq . is_enabled_sequence M s seq \<and> io = get_io seq"
@@ -107,6 +152,17 @@ lemma observable_alt : "well_formed M \<Longrightarrow> observable M \<Longright
   apply auto
   by (metis (mono_tags, lifting) Collect_cong singleton_conv)
 
+lemma get_io_length : "length seq = length (get_io seq)"
+  apply (induction seq)
+  apply auto
+  done
+
+lemma get_io_length_eq : 
+  assumes "get_io seq1 = get_io seq2"
+  shows "length seq1 = length seq2"
+  by (metis assms get_io_length)
+  
+(*
 lemma get_io_length : "
   well_formed M 
   \<Longrightarrow> is_enabled_sequence M s seq 
@@ -170,7 +226,7 @@ lemma observable_sequence1e : "
   \<Longrightarrow> (a1#S1) = S2"
   apply (cases a1)
   by (auto simp: well_formed_def observable_def)
-
+*)
 
 lemma first_list_diff_zip :
   fixes zipSeq :: "('a * 'a) list"
@@ -213,11 +269,27 @@ proof -
   then show ?thesis by auto
 qed
 
-lemma sequence_state_last_first :
-  assumes en : "is_enabled_sequence M s seq"
+lemma sequence_source_target :
+  assumes en : "is_sequence M seq"
   assumes idx: "(Suc j) < length seq"
-  shows "snd (snd (snd (seq ! j))) = fst (seq ! (Suc n))"
-  apply sledgehammer
+  shows "t_target (seq ! j) = t_source (seq ! (Suc j))"
+proof -
+  have "is_sequence M (drop j seq)" using assms sequence_drop by auto
+  moreover have "(seq ! j) = hd (drop j seq)" by (metis hd_drop_conv_nth idx lessI less_le_trans order.strict_implies_order)
+  moreover have "(seq ! (Suc j)) = hd (tl (drop j seq))" using assms by (metis drop_Suc hd_drop_conv_nth tl_drop)
+  ultimately show ?thesis using assms by (metis Cons_nth_drop_Suc Suc_lessD is_sequence.simps(3))
+qed 
+
+lemma get_io_index :
+  assumes en : "get_io seq1 = get_io seq2"
+  assumes idx: "j < length seq1"
+  shows "t_input (seq1 ! j) = t_input (seq2 ! j) \<and> t_output (seq1 ! j) = t_output (seq2 ! j)"
+proof 
+  have "map t_input seq1 = map t_input seq2" using en by (metis get_io.simps length_map map_fst_zip)
+  then show "t_input (seq1 ! j) = t_input (seq2 ! j)" by (metis idx map_eq_imp_length_eq nth_map)
+  have "map t_output seq1 = map t_output seq2" using en by (metis get_io.simps length_map map_snd_zip)
+  then show "t_output (seq1 ! j) = t_output (seq2 ! j)" by (metis idx map_eq_imp_length_eq nth_map)
+qed
 
 theorem test:
   assumes wf:"well_formed M" 
@@ -229,7 +301,7 @@ theorem test:
     "seq1 = seq2"
 proof (rule ccontr)
   assume ineq: "seq1 \<noteq> seq2"
-  have sameLength : "length seq1 = length seq2" by (meson e1 e2 get_io_length io local.wf)
+  have sameLength : "length seq1 = length seq2" by (metis get_io_length io)
   then obtain mdi where mdi_def : (* minimal difference index *)
         "mdi < length seq1 
           \<and> seq1 ! mdi \<noteq> seq2 ! mdi 
@@ -238,14 +310,15 @@ proof (rule ccontr)
   let ?equalInit = "take mdi seq1"
   let ?diff1 = "seq1 ! mdi"
   let ?diff2 = "seq2 ! mdi"
-  have io_eq_index : "\<forall> i . (fst (snd (seq1 ! i)) = fst (snd (seq1 ! i))) \<and> (fst (snd (snd (seq1 ! i))) = fst (snd (snd (seq1 ! i))))" by simp
+  
   have "False"
   proof (cases mdi)
     case 0
     obtain s1 xa ya s2 where diff1_def : "seq1 ! mdi = (s1,xa,ya,s2)" using prod_cases4 by blast
     obtain t1 xb yb t2 where diff2_def : "seq2 ! mdi = (t1,xb,yb,t2)" using prod_cases4 by blast
-    have "s1 = t1" by (metis "0" diff1_def diff2_def e1 e2 hd_conv_nth is_enabled_sequence.simps(2) length_0_conv list.exhaust_sel mdi_def sameLength)
-    moreover have "xa = xb" using assms by (smt "0" Suc_length_conv gr0_conv_Suc mdi_def nth_Cons_0 observable_sequence1d2 sameLength)
+    have "s1 = t1" using assms by (metis "0" diff1_def diff2_def fst_conv is_enabled_sequence.elims(2) list.size(3) mdi_def nth_Cons_0 sameLength)
+    moreover have "xa = xb" using diff1_def diff2_def get_io_index mdi_def by fastforce
+      (*using assms by (smt "0" Suc_length_conv gr0_conv_Suc mdi_def nth_Cons_0 observable_sequence1d2 sameLength)*)
     moreover have "ya = yb" using assms by (smt "0" Suc_length_conv gr0_conv_Suc mdi_def nth_Cons_0 observable_sequence1d2 sameLength)
     moreover have "s2 = t2" using assms by (smt "0" Suc_length_conv gr0_conv_Suc mdi_def nth_Cons_0 observable_sequence1d2 sameLength)
     ultimately have "seq1 ! mdi = seq2 ! mdi" by (simp add: diff1_def diff2_def)
@@ -258,8 +331,11 @@ proof (rule ccontr)
     obtain t1 xb yb t2 where diff2_def : "seq2 ! mdi = (t1,xb,yb,t2)" using prod_cases4 by blast
     obtain s0 xp yp s1p where same_def : "seq1 ! lastSame = (s0,xp,yp,s1p)" using prod_cases4 by blast
     have same_last : "seq1 ! lastSame = seq2 ! lastSame" using mdi_def by (simp add: Suc)
-    have "s1 = s1p" using e1 by sledgehammer
-
+    
+    have io_eq_index : "t_input (seq1 ! mdi) = t_input (seq2 ! mdi)" using assms by sledgehamme
+    have "s1 = s1p" using assms sequence_source_target by (metis Suc diff1_def is_enabled_sequence.elims(2) list.size(3) mdi_def not_less0 same_def t_source.simps t_target.simps)
+    moreover have "xa = xb" using assms by sledgehamme
+    moreover have "ya = yb" using assms by (smt "0" Suc_length_conv gr0_conv_Suc mdi_def nth_Cons_0 observable_sequence1d2 sameLength)
     then show ?thesis sorry
   qed
   
