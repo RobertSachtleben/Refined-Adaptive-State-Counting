125523d1ef13b43b8d2302f0a29addf9731d9391
RobertSachtleben <r.sachtleben@online.de>
2019-08-11 21:21:02 +0200

    Added Proof sketches

--- FSM/State_Separator.thy
+++ FSM/State_Separator.thy
@@ -1701,7 +1701,9 @@ value "is_r_distinguishable M_ex_9 0 1"
 
 
 
-
+lemma list_append_subset3 : "set xs1 \<subseteq> set ys1 \<Longrightarrow> set xs2 \<subseteq> set ys2 \<Longrightarrow> set xs3 \<subseteq> set ys3 \<Longrightarrow> set (xs1@xs2@xs3) \<subseteq> set(ys1@ys2@ys3)" by auto
+lemma subset_filter : "set xs \<subseteq> set ys \<Longrightarrow> set xs = set (filter (\<lambda> x . x \<in> set xs) ys)"
+  by auto
 
 (* Note: requires observability, a (per definition) states in non-observable FSMs may still be r-d, but this might require different inputs *)
 lemma state_separator_from_r_distinguishable_k :
@@ -1722,14 +1724,43 @@ proof (rule ccontr)
   let ?tsLeft = "(map (\<lambda> qqt . (Inl (fst qqt), t_input (snd qqt), t_output (snd qqt), Inr q1 )) (filter (\<lambda> qqt . t_source (snd qqt) = fst (fst qqt) \<and> (r_distinguishable_k_least M q1 q2 (size (product (from_FSM M q1) (from_FSM M q2))) = Some (0,t_input (snd qqt)))) (concat (map (\<lambda> qq' . map (\<lambda> t . (qq',t)) (transitions M)) (nodes_from_distinct_paths (product (from_FSM M q1) (from_FSM M q2)))))))"
   let ?tsRight = "(map (\<lambda> qqt . (Inl (fst qqt), t_input (snd qqt), t_output (snd qqt), Inr q2 )) (filter (\<lambda> qqt . t_source (snd qqt) = snd (fst qqt) \<and> (r_distinguishable_k_least M q1 q2 (size (product (from_FSM M q1) (from_FSM M q2))) = Some (0,t_input (snd qqt)))) (concat (map (\<lambda> qq' . map (\<lambda> t . (qq',t)) (transitions M)) (nodes_from_distinct_paths (product (from_FSM M q1) (from_FSM M q2)))))))"
 
-  have "set ?tsInl \<subseteq> set (shifted_transitions M q1 q2)"
+  let ?S = "?CSep \<lparr> transitions := filter (\<lambda> t . t \<in> set (?tsInl @ ?tsLeft @ ?tsRight)) (transitions ?CSep) \<rparr>"
+
+  have tsInl_subset: "set ?tsInl \<subseteq> set (shifted_transitions M q1 q2)"
     sorry
-  have "set ?tsLeft \<subseteq> set (distinguishing_transitions_left M q1 q2)"
+  have tsLeft_subset: "set ?tsLeft \<subseteq> set (distinguishing_transitions_left M q1 q2)"
     sorry
-  have "set ?tsRight \<subseteq> set (distinguishing_transitions_right M q1 q2)"
+  have tsRight_subset: "set ?tsRight \<subseteq> set (distinguishing_transitions_right M q1 q2)"
     sorry
 
+  have "set (?tsInl @ ?tsLeft @ ?tsRight) \<subseteq> set (shifted_transitions M q1 q2
+          @ distinguishing_transitions_left M q1 q2
+          @ distinguishing_transitions_right M q1 q2)"
+    using list_append_subset3[OF tsInl_subset tsLeft_subset tsRight_subset] by assumption 
+  then have "set (?tsInl @ ?tsLeft @ ?tsRight) \<subseteq> set (transitions ?CSep)"
+    unfolding canonical_separator_def
+    by (metis select_convs(4)) 
+  have "set (?tsInl @ ?tsLeft @ ?tsRight) = set (filter (\<lambda> t . t \<in> set (?tsInl @ ?tsLeft @ ?tsRight)) (transitions ?CSep))" 
+    using subset_filter[OF \<open>set (?tsInl @ ?tsLeft @ ?tsRight) \<subseteq> set (transitions ?CSep)\<close>] by assumption
+  then have "is_submachine ?S ?CSep" 
+    using transition_filter_submachine by metis
 
+  have "deadlock_state ?S (Inr q1)"
+    sorry
+  have "deadlock_state ?S (Inr q2)"
+    sorry
+
+  have "\<And> p t q1' q2'  . path ?S (Inl (q1',q2')) (p@[t]) \<Longrightarrow> t_target t = Inr q1 \<or> t_target t = Inr q2 \<Longrightarrow> \<exists> p' . is_least_r_d_k_path M q1' q2' p' \<and> map fst p = (map Inl (map fst p'))"
+  proof - 
+    fix p t q1' q2' assume "path ?S (Inl (q1',q2')) (p@[t])" and "t_target t = Inr q1 \<or> t_target t = Inr q2"
+    then show "\<exists> p' . is_least_r_d_k_path M q1' q2' p' \<and> map fst p = (map Inl (map fst p'))"
+    proof (induction p arbitrary: q1' q2' rule: rev_induct)
+      case Nil
+      then show ?case sorry
+    next
+      case (snoc x xs)
+      then show ?case sorry
+    qed
 
 (* TODO:
  - r_dist \<Longrightarrow> EX SSep (prove via r_compatible)
