c0cf919099e14d06a05f97b403b6977c8983b80f
Robert Sachtleben <r.sachtleben@online.de>
2019-08-14 11:40:42 +0200

    Reworked Fail-set contruction

--- FSM/State_Separator.thy
+++ FSM/State_Separator.thy
@@ -1613,26 +1613,67 @@ shows "\<exists> io \<in> Fail . io \<in> (LS M' q' - LS M q)"
 
 definition state_separation_fail_sequence_set_from_state_separator :: "('a, 'c) FSM_scheme \<Rightarrow> 'a \<Rightarrow> ('b + 'a, 'c) FSM_scheme \<Rightarrow> (Input \<times> Output) list set" where
   "state_separation_fail_sequence_set_from_state_separator M q1 S = 
-      output_completion_for_FSM M (L S) - {io | io p . path S (initial S) p \<and> target p (initial S) = Inr q1 \<and> (\<exists> io' . p_io p = io@io')}"
-(*      output_completion_for_FSM M {p_io p | p . path S (initial S) p \<and> (\<exists> p' . path S (initial S) (p@p') \<and> target (p@p') (initial S) = Inr q2)}" *)
-(*    output_completion_for_FSM M (prefix_completion {p_io p | p . path S (initial S) p \<and> target p (initial S) = Inr q2})" *)
+        output_completion_for_FSM M (L S) - (LS M q1)"
+(*      output_completion_for_FSM M (L S) - {io | io p . path S (initial S) p \<and> target p (initial S) = Inr q1 \<and> (\<exists> io' . p_io p = io@io')}"*)
+(* TODO: prove equivalency with above alternative definition? *)
+(* TODO: prove code *)
 
 lemma state_separation_fail_sequence_set_from_state_separator :
-  assumes "is_state_separator_from_canonical_separator (canonical_separator M q1 q2) q1 q2 S"
-      and "observable M"
-      and "q1 \<in> nodes M"
-      and "q2 \<in> nodes M"
-      and "q1 \<noteq> q2"
-    shows "fail_sequence_set M q1 (state_separation_fail_sequence_set_from_state_separator M q1 S)" (is "fail_sequence_set M q1 ?SS")
-  sorry
+  "fail_sequence_set M q1 (state_separation_fail_sequence_set_from_state_separator M q1 S)"
+  unfolding fail_sequence_set_def state_separation_fail_sequence_set_from_state_separator_def by blast
+
+
   
 lemma x :
   assumes "completely_specified M'"
-      and "inputs M' = inputs M"
-      and "outputs M' = outputs M"
+      and "set (inputs M) \<subseteq> set (inputs M')"
+      and "set (outputs M) = set (outputs M')"
       and "is_state_separator_from_canonical_separator (canonical_separator M q1 q2) q1 q2 S" 
       and "q1' \<in> nodes M'"
     shows "LS M' q1' \<inter> output_completion_for_FSM M (L S) \<noteq> {}" 
+proof -
+  obtain p where "path S (initial S) p" and "target p (initial S) = Inr q1"
+    using assms(4) path_to_node unfolding is_state_separator_from_canonical_separator_def by metis
+  moreover have "initial S = Inl (q1,q2)"
+    using assms(4) unfolding is_state_separator_from_canonical_separator_def is_submachine.simps canonical_separator_simps(1) product_simps(1) from_FSM_simps(1) by presburger
+  ultimately have "p \<noteq> []"
+    by force
+  then have "p = [hd p] @ (tl p)"
+    by auto
+  then have "path S (initial S) [hd p]"
+    using \<open>path S (initial S) p\<close> using path_prefix by metis
+  then have "p_io [hd p] \<in> L S"  
+    unfolding LS.simps by force
+  then have "\<exists> y . [(t_input (hd p), y)] \<in> (L S)"
+    by (metis (no_types, lifting) list.simps(8) list.simps(9))
+  moreover have "set (outputs S) = set (outputs M)"
+    using assms(4) unfolding is_state_separator_from_canonical_separator_def is_submachine.simps canonical_separator_simps(3) product_simps(3) by presburger
+  ultimately have "\<forall> y \<in> set (outputs M') . [(t_input (hd p), y)] \<in> output_completion_for_FSM M (L S)"
+    using assms(3) unfolding output_completion_for_FSM_def by simp
+  
+  have "t_input (hd p) \<in> set (inputs S)"
+    using \<open>path S (initial S) [hd p]\<close> by auto
+  moreover have "set (inputs S) = set (inputs M)"
+    using assms(4) unfolding is_state_separator_from_canonical_separator_def is_submachine.simps canonical_separator_simps(2) product_simps(2) by force
+  ultimately have "t_input (hd p) \<in> set (inputs M')"
+    using assms(2) by blast
+  then obtain t' where "t' \<in> h M'" and "t_source t' = q1'" and "t_input t' = t_input (hd p)"
+    using assms(1,5) unfolding completely_specified.simps by blast
+  then have "path M' q1' [t']" by auto
+  then have "[(t_input (hd p), t_output t')] \<in> LS M' q1'"
+    using \<open>t_input t' = t_input (hd p)\<close> unfolding LS.simps by force 
+
+  have "t_output t' \<in> set (outputs M')" 
+    using \<open>t' \<in> h M'\<close> by auto
+  then have "[(t_input (hd p), t_output t')] \<in> output_completion_for_FSM M (L S)"
+    using \<open>\<forall> y \<in> set (outputs M') . [(t_input (hd p), y)] \<in> output_completion_for_FSM M (L S)\<close> by blast
+
+  then show ?thesis
+    using \<open>[(t_input (hd p), t_output t')] \<in> LS M' q1'\<close> by blast
+qed
+    
+
+    
 
 lemma y :
   "path S (initial S) p \<and> target p (initial S) = Inr q1 \<and> (\<exists> io' . p_io p = io@io') \<Longrightarrow> io \<in> LS M q1"
